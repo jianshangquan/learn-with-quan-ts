89fa0a21dcaa33bc60c8ca132551d43c8a1f81ba
简尚全 <jianshangquan3@gmail.com>
2025-06-27 20:11:23 +0630

    OOP

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
+import { convertBurmeseGoldToGram, convertBurmeseGoldToMetricWeight } from "./gold-calculation";
+import { BurmeseGoldWeight, MetricWeight, TroyWeight } from "./gold-model";
+
 console.log('Hello World');
+
+const pawnItem = new BurmeseGoldWeight(1, 0, 0)
+const internationalGoldBar = new MetricWeight(1000);
+const ukGoldBar = new TroyWeight(10);
+
+const gram = convertBurmeseGoldToGram(pawnItem);
+const r = convertBurmeseGoldToMetricWeight(pawnItem);
+console.log(r.gram)
+
